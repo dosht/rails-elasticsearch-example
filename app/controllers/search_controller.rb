a6de220a68793687fb1fa54bf6b8e774cdcd2be0
Mostafa Sameh <lambda.mostafa@gmail.com>
2014-07-19 01:27:39 +0200

    basic search example

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,9 @@
+class SearchController < ApplicationController
+  def search
+    if params[:q].nil?
+      @articles = []
+    else
+      @articles = Article.search params[:q]
+    end
+  end
+end
---
+++ app/models/article.rb
@@ -0,0 +1,45 @@
+require 'elasticsearch/model'
+
+class Article < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'english', index_options: 'offsets'
+      indexes :text, analyzer: 'english'
+    end
+  end
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^10', 'text']
+          }
+        },
+        highlight: {
+          pre_tags: ['<em>'],
+          post_tags: ['</em>'],
+          fields: {
+            title: {},
+            text: {}
+          }
+        }
+      }
+    )
+  end
+end
+
+# Delete the previous articles index in Elasticsearch
+Article.__elasticsearch__.client.indices.delete index: Article.index_name rescue nil
+
+# Create the new index with the new mapping
+Article.__elasticsearch__.client.indices.create \
+  index: Article.index_name,
+  body: { settings: Article.settings.to_hash, mappings: Article.mappings.to_hash }
+
+# Index all article records from the DB to Elasticsearch
+Article.import
